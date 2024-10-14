dd7bd4c6cb782f71e2d1d337fff4bdab5f40a6ab
rtblanco <ronnytoribio1@hotmail.com>
2024-10-13 21:43:35 -0400

    removed items and learned about transposing matrix

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function rotateMatrix(matrix) {
+  // Transpose the matrix
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
 
-setupCounter(document.querySelector('#counter'))
+  // Reverse each row
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].reverse();
+  }
+
+  return matrix;
+}
+
+const matrix = [
+  [0, 0, 0],
+  [0, 0, 1],
+  [1, 1, 1]
+];
+
+console.log(rotateMatrix(matrix));
